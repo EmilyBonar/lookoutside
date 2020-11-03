9ffb26a7a24efafc12211273cf74e49c9a389e48
emilybonar <emilykbonar@gmail.com>
2020-11-03 10:31:14 -0600

    Fetch COVID data

---
+++ functions/getCovidData.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event, context) => {
+	const county = event.queryStringParameters.county;
+	const responseData = await axios
+		.get(`https://disease.sh/v3/covid-19/nyt/counties/${county}?lastdays=7`)
+		.then((response) => {
+			return response.data;
+		});
+	console.log(responseData);
+	return {
+		statusCode: 200,
+		body: JSON.stringify(responseData),
+	};
+};

9838b070b6199cc304f4452c133651bfabc06b22
emilybonar <emilykbonar@gmail.com>
2020-11-03 10:06:02 -0600

    created geolocation function

---
+++ functions/getLocationData.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event, context) => {
+	const location = event.queryStringParameters.loc;
+	const geoKey = process.env.geoKey;
+	const responseData = await axios
+		.get(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${geoKey}`,
+		)
+		.then((response) => {
+			return response.data;
+		});
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(responseData),
+	};
+};

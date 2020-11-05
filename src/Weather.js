c1876b25399e242fd54a0ceea744cb04b02bd32a
emilybonar <emilykbonar@gmail.com>
2020-11-05 09:51:11 -0600

    Add current weather status

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,21 @@ function Weather(props) {
 			<div className="text-center">
 				<h2 className="text-2xl">Current temp</h2>
 				<h1 className="font-bold text-5xl">{props.data.current.temp}°F</h1>
-				<h3 className="">Feels like {props.data.current.feels_like}°F</h3>
+				<h3 className="">
+					Feels like{" "}
+					<span className="font-bold">{props.data.current.feels_like}°F</span>
+				</h3>
+				<h2>
+					The weather is currently{" "}
+					<span className="font-bold">
+						{props.data.current.weather[0].description}
+					</span>
+				</h2>
+				<img
+					className="mx-auto"
+					src={`http://openweathermap.org/img/wn/${props.data.current.weather[0].icon}@2x.png`}
+					alt="Current Weather Icon"
+				/>
 			</div>
 		);
 	} else {
